4a9ccda0f71b32095d965dbd712eb3a9d6f550e4
Ian McBurnie <imcburnie@ebay.com>
2016-06-17 14:37:12 -0700

    jquery 3 now allowed

--- jquery.tooltip.js
+++ jquery.tooltip.js
@@ -2,7 +2,7 @@
 * @file jQuery plugin that creates the basic interactivity for an ARIA tooltip widget
 * @desc this plugin is in early experimental status
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.0.3
+* @version 0.1.0
 * @requires jquery
 * @requires jquery-stick
 * @requires jquery-focusable
